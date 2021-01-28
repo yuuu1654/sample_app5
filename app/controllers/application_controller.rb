fe7ba720f2727d65a45d6631b00665ce4f0a4853
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2021-01-28 07:07:07 +0000

    Finish ch3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   
-  def hello
-    render html: "hi!!!"
-  end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  # => StaticPages#home
+  
   get 'static_pages/help'
-  root 'application#hello'
+  # => StaticPages#help
+  
+  get 'static_pages/about'
+  root 'static_pages#home'
 end

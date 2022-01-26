05a8493249c0bfcda77dec1e4a0d31ddd909fd76
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2022-01-26 17:51:11 +0900

    modified user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -134,5 +134,7 @@ class User < ApplicationRecord
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+    #ユーザーモデルを編集しました
   
 end
